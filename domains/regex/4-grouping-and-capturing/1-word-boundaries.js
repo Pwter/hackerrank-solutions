79e2669e01f52f92f3de380d3f34250d8de6dcf4
Pwter <dakcsi@gmail.com>
2016-02-09 16:50:58 +0000

    grouping and capturing

---
+++ domains/regex/4-grouping-and-capturing/1-word-boundaries.js
@@ -0,0 +1,2 @@
+var Regex_Pattern = /\b[aeiouAEIOU][A-Za-z]*\b/; //Do not delete '/'. Replace __________ with your regex. 
+
---
+++ domains/regex/4-grouping-and-capturing/3-alternative-matching.js
@@ -0,0 +1,2 @@
+var Regex_Pattern = /^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[A-Za-z]+$/; //Do not delete '/'. Replace __________ with your regex. 
+
